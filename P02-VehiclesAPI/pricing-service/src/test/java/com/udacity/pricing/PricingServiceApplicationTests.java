6c8e2c7b5e1d663950ace6307418f2af5e2470be
Quang <quang@example.com>
2023-05-03 21:14:34 +0700

    microservice

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,36 @@
-package com.udacity.pricing;
+package com.udacity.pricing.test.web;
 
+import com.udacity.pricing.api.PricingController;
+import com.udacity.pricing.service.PricingService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(PricingController.class)
 public class PricingServiceApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
+	@MockBean
+	PricingService pricingService;
 
 	@Test
-	public void contextLoads() {
-	}
+	public void testPriceApi() throws Exception{
+		mockMvc.perform(get("/services/price?vehicleId=1"))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("vehicleId").value(1L))
+				.andExpect(MockMvcResultMatchers.jsonPath("price").isNotEmpty())
+				.andExpect(MockMvcResultMatchers.jsonPath("currency").value("USD"));
 
+	}
 }
